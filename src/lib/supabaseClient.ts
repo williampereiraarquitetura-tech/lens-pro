eb8b2627cd2c0e7d458ea84215d0ccb299140d46
williampereiraarquitetura-tech <williampereira.arquitetura@gmail.com>
2026-05-18 09:14:58 -0300

    Estrutura do app LENS PRO

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Pegue essas chaves na aba "API" do seu painel do Supabase
-const supabaseUrl = 'https://vlvmiusswdkghiqihimx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsdm1pdXNzd2RrZ2hpcWloaW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5ODYxMTksImV4cCI6MjA5NDU2MjExOX0.K_sf3l89MHfDkr6Zvnjo9ttltfoZFll_ujvLe4PMABM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Chaves do Supabase não encontradas! Verifique seu arquivo .env");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
